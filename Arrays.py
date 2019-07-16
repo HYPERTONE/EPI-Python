e2a186ae58ed9e9406065d2d16baf839e9a8d545
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-16 16:19:04 -0400

    Create Arrays.py

---
+++ Arrays.py
@@ -0,0 +1,17 @@
+# The simplest data structure is the array, which is a contiguous block of memory. It is usually used to represent sequences.
+
+# When working with arrays you should take advantage of the fact that you can operate efficiently on both ends. For this problem,
+# we can partition the array into three subarrays: Even, Unclassified, and Odd, appearing in that order.
+
+def evenOdd(A):
+    nextEven, nextOdd = 0, len(A) - 1
+    while nextEven < nextOdd:
+        if A[nextEven] % 2 == 0:
+            nextEven += 1
+        else:
+            A[nextEven], A[nextOdd] = A[nextOdd], A[nextEven]
+            nextOdd -= 1
+            
+sample = [5, 6, 10, 5, 7, 1, 2]
+evenOdd(sample)
+print(sample) -> [2, 6, 10, 7, 1, 5, 5]
