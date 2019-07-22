22679a1379f502a2c18c91fd8873e0a0501632b8
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-21 20:21:51 -0400

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,7 @@
+Strings are ubiquitous in programming today - scripting, web development, and bioinformatics all make extensive use of strings.
+
+You should know how strings are represented in memory, and understand basic operations on strings such as comparison,
+copying, joining, splitting, matching, etc. Advanced string processing algorithms often use hash tables (Chapter 12) and
+dynamic programming (Page 234).
+
+A palindromic string is one which reads the same when it is reversed.
