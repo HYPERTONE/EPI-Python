eaf34d6edd70600c370e732e58b17416bea2d83c
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-22 13:05:52 -0400

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -1,7 +1,18 @@
-Strings are ubiquitous in programming today - scripting, web development, and bioinformatics all make extensive use of strings.
+# Strings are ubiquitous in programming today - scripting, web development, and bioinformatics all make extensive use of strings.
 
-You should know how strings are represented in memory, and understand basic operations on strings such as comparison,
-copying, joining, splitting, matching, etc. Advanced string processing algorithms often use hash tables (Chapter 12) and
-dynamic programming (Page 234).
+# You should know how strings are represented in memory, and understand basic operations on strings such as comparison,
+# copying, joining, splitting, matching, etc. Advanced string processing algorithms often use hash tables (Chapter 12) and
+# dynamic programming (Page 234).
 
-A palindromic string is one which reads the same when it is reversed.
+# A palindromic string is one which reads the same when it is reversed.
+
+
+# 6.1 - Interconvert Strings And Integers
+
+
+# Implement an integer to string conversion function, an da string to integer conversion function. For example, if the input to the first
+# function is the integer 314, it should return the string '314' and if the input to the second function is the string '314' it should
+# return the integer 314.
+
+
+
