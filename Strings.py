28345918304b5d6c033d0ef23d4212a25203b05f
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-22 22:11:59 -0400

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -83,15 +83,16 @@ print(convert_base(thestring, 7, 13))
 # Implement a function that converts a spreadsheet column id to the corresponding integer, with "A" corresponding to 1.
 # For example, you should return 4 for "D", 27 for "AA", 702 for "ZZ", etc.
 
-alphabet = ['Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+def intSpreadsheet(colNum):
+    s = ""
+    
+    while colNum > 0:                               # 
+        colNum, remainder = divmod(colNum - 1, 26)  # x = num - 1, y = 26; colNum = quotient, remainder = remainder
+        s = chr(65 + remainder) + s                 # 65 = A, 66 = B, etc.
+    return s
 
-# multiplier = math.ceil(702 / len(alphabet))
+intSpreadsheet(702) # -> 'ZZ'
 
-multiplier = 704
-
-while multiplier > 26:
-    multiplier = math.ceil(multiplier / 26)
-    print(multiplier)
-    if multiplier <= 26:
-        break
+# For 702, we divmod(701, 26) which results in quotient = 26 with remainder = 25. Second iteration results in
+# divmod(25, 26) which results in quotient = 0 with remainder = 25. After 2 iterations s = 'ZZ'
+# chr(65) = 'A' + 25 gives chr(90) = 'Z'
