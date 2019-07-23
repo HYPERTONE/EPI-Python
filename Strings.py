bd3e37e91b56c27cfbf8a0360b83183bb7fcbef8
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-23 15:02:30 -0400

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -148,3 +148,21 @@ def palindromitic(s):
 palindromitic("A man, a plan, a canal, Panama.") # -> True
 
 
+# 6.6 - Reverse All The Words In A Sentence
+
+# Given a string containing a set of words separated by whitespace, we would like to transform it to a string in which the words 
+# appear in the reverse order. For example, "Alice likes Bob" transforms to "Bob likes Alice". We don't need to keep the original string.
+
+# Implement a function for reversing the words in a string s.
+
+def reverseString(s):
+    
+    newString = s[::-1].split(' ')
+    
+    f = ''
+    for word in newString:
+        f = f + word[::-1] + ' '
+    
+    print(f)
+    
+reverseString('ram is costly') # -> costly is ram
