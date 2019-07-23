1d4947d67b8560913e86efbaed8b951b022a65b4
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-23 13:39:32 -0400

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -125,3 +125,26 @@ def replaceRemove(a, size):
 
 arr = ['a', 'b', 'c', 'a', 'd']
 replaceRemove(arr, 4) # -> ['d', 'd', 'c', 'd']
+
+
+# 6.5 - Test Palindromicity
+
+# Imlement a function which takes as input a string s and returns true if s is a palindromic string.
+
+# For example, "A man, a plan, a canal, Panama." and "Able was I, ere I saw Elba" are palindromic, but "Ray a Ray" is not.
+
+def palindromitic(s):
+    
+    chars = [',', '.', '!', ' ']
+    for c in chars:
+        s = s.replace(c, '')
+    
+    if s.lower() == s[::-1].lower():
+        return True
+    else:
+        return False
+
+    
+palindromitic("A man, a plan, a canal, Panama.") # -> True
+
+
