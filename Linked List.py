4b6c3c7713d8bf63561ab26e2b7d608d42f5627b
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-25 15:45:29 -0400

    Update Linked List.py

--- Linked List.py
+++ Linked List.py
@@ -10,3 +10,20 @@
 # in a list has time complexity O(1). On the other hand, obtaining the kth element in a list is expensive, having O(n) time complexity.
 # Lists are usually building blocks of more complex data structures.
 
+# There are two tpyes of list-related problems - those where you have to implement your own list, and those where you have to exploit
+# the standard list library.
+
+
+
+# 7.1 - Merge Two Sorted Lists
+
+# Consider two single linked lists in which each node holds a number. Assume the lists are sorted, i.e., numbers in the lists appear
+# in ascending order within each list. The merge of the two lists is a list consisting of the nodes of the two lists in which numbers 
+# appear in ascending order.
+
+# Write a program that takes two lists, assumed to be sorted, and returns their merge. The only field your program can change in a node 
+# is its next field.
+
+
+
+
