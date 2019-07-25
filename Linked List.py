a0bb0d8fccbb27250053ed8b8d8695016da56bbf
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-25 15:30:54 -0400

    Update Linked List.py

--- Linked List.py
+++ Linked List.py
@@ -1,1 +1,12 @@
+# A list implements an ordered collection of values, which may include reptitions. Specifically, a singly linked list is a data 
+# structure that contains a sequence of nodes such that each node contains an object and a reference to the next node in the list.
+
+# The first node is referred to as the head and the last node is referred to as the tail; the tail's next field is null.
+
+# In a Double Linked List, each node has a link to its predecessor, similarily, a sentinel node or a self-loop can be used instead of null
+# to mark the end of the list.
+
+# A list is similar to an array in that it contains objects in linear order. The key difference are that inserting and deleting elements
+# in a list has time complexity O(1). On the other hand, obtaining the kth element in a list is expensive, having O(n) time complexity.
+# Lists are usually building blocks of more complex data structures.
 
