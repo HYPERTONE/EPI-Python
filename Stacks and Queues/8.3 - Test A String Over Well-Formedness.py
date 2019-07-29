47e6d354912649f54fe9f2cbb7797cdbd48181ec
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-29 16:29:14 -0400

    Update 8.3 - Test A String Over Well-Formedness.py

--- Stacks and Queues/8.3 - Test A String Over Well-Formedness.py
+++ Stacks and Queues/8.3 - Test A String Over Well-Formedness.py
@@ -5,3 +5,23 @@
 
 # Write a program that tests if a string made up of the characters '(',')','[',']','{', and '}' is well-formed.
 
+def wellFormed(s):
+    
+    LOOKUP = {
+            '{' : '}',
+            '(' : ')',
+            '[' : ']'
+             }
+    
+    leftChars = []
+    for c in s:
+        if c in LOOKUP:
+            leftChars.append(c)
+            print(leftChars)
+        elif not leftChars or LOOKUP[leftChars.pop()] != c: # if leftChars is empty OR
+            print('not found: {}'.format(leftChars)) # if a ( exists, we append it (since its in the lookup) and then we 
+            return False                             # pop it and compare it to the previous stack item
+
+    return not leftChars
+
+wellFormed('{{(}}') # -> True
