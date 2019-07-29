3d9dcdb7b9354c0c2e9265b1573ba7e5b7badfa4
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-28 20:27:50 -0400

    Update 8.1 - Implement A Stack with Max API.py

--- Stacks and Queues/8.1 - Implement A Stack with Max API.py
+++ Stacks and Queues/8.1 - Implement A Stack with Max API.py
@@ -11,3 +11,37 @@
 # Design a stack that includes a max operation, in addition to push and pop. The max method should return the maximum
 # value stored in the stack.
 
+import collections
+
+class Stack:
+    ElementWithCachedMax = collections.namedtuple('ElementWithCachedMax', ('element', 'max'))
+
+    def __init__(self):
+        self._element_with_cached_max = []
+
+    def empty(self):
+        return len(self._element_with_cached_max) == 0
+
+    def max(self):
+        if self.empty():
+            raise IndexError('max(): empty stack')
+        return self._element_with_cached_max[-1].max
+
+    def pop(self):
+        if self.empty():
+            raise IndexError('pop(): empty stack')
+        return self._element_with_cached_max.pop().element
+
+    def push(self, x):
+        self._element_with_cached_max.append(
+            self.ElementWithCachedMax(x, x if self.empty() else max(
+                x, self.max())))
+        
+        
+newStack = Stack()
+newStack.push(1)
+newStack.push(3)
+newStack.push(9)
+print(newStack.max())
+print(newStack.pop())
+print(newStack.max())
