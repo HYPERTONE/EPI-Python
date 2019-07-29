9df452e48141de82be97d837514681263ca3a5a6
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-28 20:05:41 -0400

    Update 8.1 - Implement A Stack with Max API.py

--- Stacks and Queues/8.1 - Implement A Stack with Max API.py
+++ Stacks and Queues/8.1 - Implement A Stack with Max API.py
@@ -4,3 +4,10 @@
 
 # A stack supports two basic operations - push and pop. Elements are added (pushed) and removed (popped) in last-in, 
 # first-out order. If the stack is empty, pop typically returns null or throws an exception.
+
+
+# 8.1 - Implement A Stack With Max API
+
+# Design a stack that includes a max operation, in addition to push and pop. The max method should return the maximum
+# value stored in the stack.
+
