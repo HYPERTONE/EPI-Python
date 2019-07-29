649f4d709a3851c53a1ffc3a921c434f7104802c
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-28 20:04:16 -0400

    Update 8.1 - Implement A Stack with Max API.py

--- Stacks and Queues/8.1 - Implement A Stack with Max API.py
+++ Stacks and Queues/8.1 - Implement A Stack with Max API.py
@@ -1,1 +1,6 @@
 
+# Stacks support last-in, first-out semantics for inserts and deletes, whereas queues are first-in, first-out. Stacks and
+# queues are usually building blocks in a solution to a complex problem.
+
+# A stack supports two basic operations - push and pop. Elements are added (pushed) and removed (popped) in last-in, 
+# first-out order. If the stack is empty, pop typically returns null or throws an exception.
