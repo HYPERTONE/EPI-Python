9db16d84e726e7a24434afa27f0b0ec61f1261b1
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-30 09:08:35 -0400

    Update Queues.py

--- Stacks and Queues/Queues.py
+++ Stacks and Queues/Queues.py
@@ -1,1 +1,13 @@
 
+# Queues
+
+# A queue supports two basic operations - enqueue and dequeue. If the queue is empty, dequeue typically returns null or throws 
+# an exception. Elements are added (enqueued) and removed (dequeued) in first-in, first-out order. The most recently inserted 
+# element is referred to as the tail or back element. 
+
+# A queue can be implemented using a linked list. A queue can also be implemented using an array.
+
+# A deque, also sometimes called a double-ended queue, is a doubly linked list in which all insertions and deletions are from one of
+# the two ends of the list, i.e., at the head or the tail. An insertion to the front is commonly called a push, and an insertion
+# to the back is commonly called an inject. A deletion from the front is commonly called a pop, and a deletion from the back is 
+# commonly called an eject.
