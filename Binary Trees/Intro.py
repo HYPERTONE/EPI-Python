ff81b89f2317ab6d9e1cf61d8b1d1356e573a3cb
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-07-31 08:17:04 -0400

    Update Intro.py

--- Binary Trees/Intro.py
+++ Binary Trees/Intro.py
@@ -1,1 +1,20 @@
 
+# A binary tree is either empty, or a root node r together with a left binary tree and a right binary tree. The subtrees themselves
+# are binary trees. The left binary tree is referred to as the left subtree of the root, and the right is the right subtree of the root.
+
+# Binary trees most commonly occur in the context of binary searches, wherein keys are stored in a sorted fashion. Binary trees are
+# appropriate when dealing with hierarchies.
+
+# For any node there exists a unique sequence of nodes from the root to that node with each node in the sequence being a child of the
+# previous node. This sequence is referred to as the search path from the root to the node.
+
+# A node is an ancestor of d if it lies on the search path from the root to do. If a node is an ancestor, we say d is a descendant of that node.
+
+# The depth of a node n is the number of nodes on the search path from the root to n, not including n itself.
+
+# The height of a binary tree is the maximum depth of any node in that tree.
+
+# A level of a tree is all nodes at the same depth.
+
+
+
