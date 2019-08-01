8b5a0ec35d18f98f1ba20605e3ae319b5e80d108
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-01 12:31:19 -0400

    Update 10.1 - Merge Sorted Files.py

--- Heaps/10.1 - Merge Sorted Files.py
+++ Heaps/10.1 - Merge Sorted Files.py
@@ -1,10 +1,4 @@
 
-# A heap is a specialized binary tree. The keys must satisfy the heap property - the key at each node is at least as great
-# as the keys stored at its children.
-
-# A heap is sometimes referred to as a priority queue because it behaves like a queue, with one difference: each element has a 
-# 'priority' associated with it, and deletion removes the element with the highest priority.
-
 # 10.1 - Merge Sorted Files
 
 # You are given 500 files, each containing stock trade information for an S&P 500 company. Each trade is encoded by a line in 
