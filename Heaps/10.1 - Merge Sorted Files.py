5d7d3022e22cff035e1f69f1eb4a63ef09258bb8
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-01 14:28:33 -0400

    Update 10.1 - Merge Sorted Files.py

--- Heaps/10.1 - Merge Sorted Files.py
+++ Heaps/10.1 - Merge Sorted Files.py
@@ -14,3 +14,14 @@
 
 # Write a program that takes as input a set of sorted sequences and computes the union of these sequences as a sorted sequence.
 # For example, if the input is <3, 5, 7>, <0, 6>, and <0, 6, 28>, then the output is <0, 0, 3, 5, 6, 6, 7, 28>.
+
+
+# This is actually pretty cool
+
+def mergeSortedArrays(arrays):
+    return list(heapq.merge(*arrays))
+    
+mergeSortedArrays([[3, 5, 7], [0, 6], [0, 6, 28]]) # -> [0, 0, 3, 5, 6, 6, 7, 28]
+
+# Note that * unpacks the list of lists. So [[3, 5, 7], [0, 6], [0, 6, 28]] becomes [3, 5, 7] [0, 6] [0, 6, 28]
+# which is a valid input in merge such as list(heapq.merge([9,2], [3,4])) will return [3, 4, 9, 2]
