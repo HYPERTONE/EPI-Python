70307c9d4f2c342835482c0d50d4a2ee34bd4107
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-01 13:08:40 -0400

    Update Intro.py

--- Heaps/Intro.py
+++ Heaps/Intro.py
@@ -6,3 +6,9 @@
 
 # A heap is sometimes referred to as a priority queue because it behaves like a queue, with one difference: each element has a 
 # 'priority' associated with it, and deletion removes the element with the highest priority.
+
+# Use a heap when all you care about is the largest or smallest elements, and you do not need to support fast lookup,
+# delete, or search operations for arbitrary elements.
+
+# A heap is a good choice when you need to compute the k largest or k smallest elements in a collection. For the former, 
+# use a min-heap, for the latter use a max-heap.
