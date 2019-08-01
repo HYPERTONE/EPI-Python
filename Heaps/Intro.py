b7fca6f5866d84b2975c8bd870e9b7cccc5e283a
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-01 12:33:43 -0400

    Update Intro.py

--- Heaps/Intro.py
+++ Heaps/Intro.py
@@ -1,4 +1,7 @@
-# A heap is a specialized binary tree. The keys must satisfy the heap property - the key at each node is at least as great
+# A heap is a specialized binary tree - specifally a complete binary tree (meaning all levels are completely filled 
+# except possibly the last level and the last level has all keys as left as possible).
+
+# The keys must satisfy the heap property - the key at each node is at least as great
 # as the keys stored at its children.
 
 # A heap is sometimes referred to as a priority queue because it behaves like a queue, with one difference: each element has a 
