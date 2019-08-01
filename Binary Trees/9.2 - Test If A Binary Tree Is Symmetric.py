977562f3952e1f13b92db9775506bb2b144d3cae
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-01 11:08:25 -0400

    Update 9.2 - Test If A Binary Tree Is Symmetric.py

--- Binary Trees/9.2 - Test If A Binary Tree Is Symmetric.py
+++ Binary Trees/9.2 - Test If A Binary Tree Is Symmetric.py
@@ -1,1 +1,6 @@
 
+# A binary tree is symmetric if you can draw a vertical line through the root and then the left subtree is a mirror image of the
+# right subtree.
+
+# Write a prgoram that checks whether a binary tree is symmetric.
+
