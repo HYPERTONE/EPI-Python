fd9f1c0c87e8fa56045d4f03a4ecbfd0ba3cabcd
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-03 00:48:19 -0400

    Update 10.3 - Sort An Almost-Sorted Array.py

--- Heaps/10.3 - Sort An Almost-Sorted Array.py
+++ Heaps/10.3 - Sort An Almost-Sorted Array.py
@@ -6,3 +6,25 @@
 # Each number is at most k away from its correctly sorted position. For example, no number in the sequence <3,-1,2,6,4,5,8>
 # is more than 2 away from its final sorted position.
 
+import itertools
+
+def sortApprox(sequence, k):
+    result = []
+    minHeap = []
+    
+    for x in itertools.islice(sequence, k):
+        heapq.heappush(minHeap, x)
+        
+    for x in sequence:
+        smallest = heapq.heappushpop(minHeap, x) # this pushes and pops the smallest element each time
+        result.append(smallest) # and appends it to result (which sorts ascendingly)
+        
+    while minHeap:
+        smallest = heapq.heappop(minHeap)
+        result.append(smallest)
+        
+    return result
+    
+    
+a = [3, -1, 2, 6, 4, 5, 8]
+sortApprox(a, 2)
