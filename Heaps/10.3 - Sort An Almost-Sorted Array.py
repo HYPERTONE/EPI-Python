31ab1a73fcf3f6edb79f7e66c6ccf8d16e7ee355
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-03 00:34:27 -0400

    Update 10.3 - Sort An Almost-Sorted Array.py

--- Heaps/10.3 - Sort An Almost-Sorted Array.py
+++ Heaps/10.3 - Sort An Almost-Sorted Array.py
@@ -1,1 +1,8 @@
 
+# Often data is almost sorted - for example, a server receives timestamped stock quotes and earlier quotes may arrive slightly
+# after later quotes because of differences in sever loads and network routes.
+
+# Write a program which takes as input a very long sequence of numbersd and prints the numbers in sorted order.
+# Each number is at most k away from its correctly sorted position. For example, no number in the sequence <3,-1,2,6,4,5,8>
+# is more than 2 away from its final sorted position.
+
