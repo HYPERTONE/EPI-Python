6f301af43569e6a7c8b266e238968006ec29c240
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-04 15:49:43 -0400

    Update 10.6 - Compute k-Largest Elements In A Max Heap.py

--- Heaps/10.6 - Compute k-Largest Elements In A Max Heap.py
+++ Heaps/10.6 - Compute k-Largest Elements In A Max Heap.py
@@ -1,1 +1,7 @@
 
+# A heap contains limited information about the ordering of elements.
+
+# Given a max-heap, represented as an array A, design an algorithm that computes the k largest elements stored in the max-heap.
+# You cannot modify the heap. 
+
+# For example <561,314,401,28,156,359,271,11,3> the four largest elements are 561, 314, 401, 359.
