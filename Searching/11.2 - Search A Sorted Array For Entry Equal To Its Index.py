94fccef6a026649eafc6b93b6642604e45e38301
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-05 11:42:21 -0400

    Update 11.2 - Search A Sorted Array For Entry Equal To Its Index.py

--- Searching/11.2 - Search A Sorted Array For Entry Equal To Its Index.py
+++ Searching/11.2 - Search A Sorted Array For Entry Equal To Its Index.py
@@ -1,1 +1,27 @@
 
+# Design an efficient algorithm that takes a sorted array of distinct integers, and returns an index i such that the 
+# element at index i equals i.
+
+# For example, if the input is <-2, 0, 2, 3, 6, 7, 9> your algorithm should return 2 or 3.
+
+def entryIndex(A):
+    
+    left = 0
+    right = len(A) - 1
+    result = -1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        difference = A[mid] - mid
+        
+        if difference == 0:
+            return mid
+        elif difference > 0:
+            right = mid - 1
+        else:
+            left = mid + 1
+            
+    return result
+            
+A = [-2, 0, 2, 3, 6, 7, 9]
+entryIndex(A) # -> 3
