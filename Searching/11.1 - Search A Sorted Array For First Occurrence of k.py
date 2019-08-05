1b33ca0bd4830a114aba55652ceac5bfeb16a807
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-05 11:14:57 -0400

    Update 11.1 - Search A Sorted Array For First Occurrence of k.py

--- Searching/11.1 - Search A Sorted Array For First Occurrence of k.py
+++ Searching/11.1 - Search A Sorted Array For First Occurrence of k.py
@@ -5,3 +5,44 @@
 # For example, given <-14, -10, 2, 108, 108, 243, 285, 285, 285, 401> the method should return 3 when given key 108 and 6 when given key 285.
 
 
+# Naive Approach (have to check each element)
+
+def firstOccurrence(A, k):
+    
+    found = -1
+    for i, j in enumerate(A):
+        if j == k:
+            found = i
+            break
+            
+    return found
+
+
+A = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
+firstOccurrence(A, 108)
+
+
+
+# Binary Approach (cut stuff in half)
+
+def firstOccurrence(A, k):
+    
+    left = 0
+    right = len(A) - 1
+    result = -1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if k < A[mid]:
+            right = mid - 1
+        elif k == A[mid]:
+            result = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+        
+    return result
+        
+A = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
+firstOccurrence(A, 285)
