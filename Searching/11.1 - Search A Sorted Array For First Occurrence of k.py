77f787cf9a5a8c2b13c37f7a941389cad4e371e4
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-05 10:18:37 -0400

    Update 11.1 - Search A Sorted Array For First Occurrence of k.py

--- Searching/11.1 - Search A Sorted Array For First Occurrence of k.py
+++ Searching/11.1 - Search A Sorted Array For First Occurrence of k.py
@@ -1,1 +1,7 @@
 
+# Binary search commonly asks for the index of any element of a sorted array that is equal to a specified element.
+
+# Write a method that takes a sorted array and a key and returns the index of the first occurrence of that key in the array. 
+# For example, given <-14, -10, 2, 108, 108, 243, 285, 285, 285, 401> the method should return 3 when given key 108 and 6 when given key 285.
+
+
