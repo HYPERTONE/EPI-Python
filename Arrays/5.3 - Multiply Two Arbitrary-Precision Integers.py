47f90936ff321c18ba3bbb275f5c6d6ae7f6dd02
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-05 14:05:32 -0400

    Update 5.3 - Multiply Two Arbitrary-Precision Integers.py

--- Arrays/5.3 - Multiply Two Arbitrary-Precision Integers.py
+++ Arrays/5.3 - Multiply Two Arbitrary-Precision Integers.py
@@ -1,1 +1,6 @@
 
+# Certain applications require arbitrary precision arithmetic. One way to achieve this is to use arrays to represent integers,
+# e.g., with one digit per array entry, with the most significant digit appearing first, and a negative leading digit denoting
+# a negative integer. For example, [-7, 6, 1, 8] represents -7618.
+
+# Write a program that takes two arrays representing integers, and returns an integer representing their product.
