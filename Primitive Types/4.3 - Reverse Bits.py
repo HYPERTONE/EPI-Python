093773210e5d2d6af92e3d27025de2a1b8aa404c
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-05 16:23:59 -0400

    Update 4.3 - Reverse Bits.py

--- Primitive Types/4.3 - Reverse Bits.py
+++ Primitive Types/4.3 - Reverse Bits.py
@@ -2,3 +2,13 @@
 # Write a program that takes a 64-bit unsigned integer and returns the 64-bit unsigned integer consisting 
 # of the bits of the input in reverse order.
 
+def reverseBit(num):
+    result = 0
+    while num:
+        result = (result << 1) + (num & 1)
+        num >>= 1
+    return result
+
+# The goal here is to AND our num by 1 and then have it shifted to the LEFT one place while
+# num is being shifted to the RIGHT. Every time we AND something, we will get a 1 when num had a 1,
+# and a 0 when num had a zero. The loop terminates because num gets shifted until it becomes 0.
