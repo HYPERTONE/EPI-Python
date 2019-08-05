ea50e01f307ca488b15c666bbff1964725ef51fc
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-05 13:38:47 -0400

    Update 11.4 - Compute The Integer Square Root.py

--- Searching/11.4 - Compute The Integer Square Root.py
+++ Searching/11.4 - Compute The Integer Square Root.py
@@ -1,1 +1,5 @@
 
+# Write a program which takes a non-negative integer and returns the largest integer whos square is less than or equal to the given integer.
+# For example, if the input is 16, return 4. If the input is 300, return 17 since 17^2 = 289 < 300 (18^2 = 324 > 300).
+
+
