5fc01a08935e8fa420e40de3ad96239b436379fe
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-05 15:06:51 -0400

    Update Intro.py

--- Primitive Types/Intro.py
+++ Primitive Types/Intro.py
@@ -1,1 +1,6 @@
 
+# A program updates variables in memory according to its instructions. 
+# Variables come in types - a type is a classification of data that spells out possible values for that type and the operations 
+# that can be performed on it. 
+
+# In Python, everything is an object - this includes Booleans, integers, characters, etc.
