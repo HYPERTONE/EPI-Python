8e83f916c50ef0351349f67281ff74cdeb6390ba
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-05 13:47:28 -0400

    Update 11.4 - Compute The Integer Square Root.py

--- Searching/11.4 - Compute The Integer Square Root.py
+++ Searching/11.4 - Compute The Integer Square Root.py
@@ -3,3 +3,20 @@
 # For example, if the input is 16, return 4. If the input is 300, return 17 since 17^2 = 289 < 300 (18^2 = 324 > 300).
 
 
+def sqrt(k):
+    
+    left = 0
+    right = k
+    
+    while left <= right:
+        mid = (left + right) // 2
+        mid_squared = mid * mid
+        
+        if mid_squared <= k:
+            left = mid + 1
+        else:
+            right = mid - 1
+            
+    return left - 1
+
+sqrt(300) # -> 17
