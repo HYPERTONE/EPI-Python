ce90bfe0c7b86e43cffb5755e58b50a98851c19d
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-06 09:38:23 -0400

    Update 6.7 - Compute All Mnemonics For A Phone Number.py

--- Strings/6.7 - Compute All Mnemonics For A Phone Number.py
+++ Strings/6.7 - Compute All Mnemonics For A Phone Number.py
@@ -3,3 +3,17 @@
 # sequences that correspond to the phone number. The cell phone keypad is specified by a mapping that takes a digit and returns the 
 # corresponding set of characters. The character sequences do not have to be legal words or phrases.
 
+def mneumonic(digits, word):
+    
+    letters = pad[digits[0]]
+    
+    if len(digits) == 1:
+        for letter in letters:
+            print(letter + word)
+    else:
+        for letter in letters:
+            mneumonic(digits[1:], word + letter)
+    
+    
+
+mneumonic('32', '')
