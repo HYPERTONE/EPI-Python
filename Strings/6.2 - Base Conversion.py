d599cadc81ebdb34ab82ae166bf33ec21d11c682
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-06 08:16:27 -0400

    Update 6.2 - Base Conversion.py

--- Strings/6.2 - Base Conversion.py
+++ Strings/6.2 - Base Conversion.py
@@ -1,1 +1,6 @@
 
+# Write a program that performs base conversion. The input is a string, an integer b1, and another integer b2. The string
+# represents an integer in base b1. The output should be the string representing the integer in base b2. 
+
+# For example, if the string is '615', b1 is 7 and b2 is 13, then the result should be '1A7', 
+# since 6 x 7^2 + 1 x 7 + 6 = 1 x 13^2 + 10 x 13 + 7.
