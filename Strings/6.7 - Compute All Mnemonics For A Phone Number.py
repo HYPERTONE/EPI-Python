523c570b1bb4eae1c4ca60b82c6007c39f9bf44b
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-06 08:28:44 -0400

    Update 6.7 - Compute All Mnemonics For A Phone Number.py

--- Strings/6.7 - Compute All Mnemonics For A Phone Number.py
+++ Strings/6.7 - Compute All Mnemonics For A Phone Number.py
@@ -1,1 +1,5 @@
 
+# Write a program which takes as input a phone number, specified as a string of digits, and returns all possible character 
+# sequences that correspond to the phone number. The cell phone keypad is specified by a mapping that takes a digit and returns the 
+# corresponding set of characters. The character sequences do not have to be legal words or phrases.
+
