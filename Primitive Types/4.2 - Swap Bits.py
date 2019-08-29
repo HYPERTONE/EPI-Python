d0e8e259cdd0d260e93407b764037d98eababc38
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-08-29 09:03:27 -0400

    Update 4.2 - Swap Bits.py

--- Primitive Types/4.2 - Swap Bits.py
+++ Primitive Types/4.2 - Swap Bits.py
@@ -11,3 +11,9 @@ def bitSwap(x, i, j):
     return x
 
 bitSwap(73, 1, 6) # If x = 73, i (MSB) has index 1, and j (LSB) has index 6 --> Output is 1011 (11) with bitmask 1000010 (66)
+
+# 73 -> 1001001 where index 1 is 1 and index 6 is 0
+# 73 -> [1]0010[0]1 we want to swap [1,6] to get 0001011 (which yields 11)
+
+# So we compute a bitmask by left shifting 1 by i,j and applying an OR
+# Then we XOR the bitmask with x to obtain our result
