1e2d284b75eecf97bdd60b2143906a4147165fd1
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-09-02 20:55:04 -0400

    Update 4.3 - Reverse Bits.py

--- Primitive Types/4.3 - Reverse Bits.py
+++ Primitive Types/4.3 - Reverse Bits.py
@@ -12,3 +12,7 @@ def reverseBit(num):
 # The goal here is to AND our num by 1 and then have it shifted to the LEFT one place while
 # num is being shifted to the RIGHT. Every time we AND something, we will get a 1 when num had a 1,
 # and a 0 when num had a zero. The loop terminates because num gets shifted until it becomes 0.
+
+
+Note that this is reversing the base 2 value of the integer. So if we input an integer of 123, its binary value is 1111011.
+The reverse of 1111011 is 1101111 which when converted to an integer is actually 111.
